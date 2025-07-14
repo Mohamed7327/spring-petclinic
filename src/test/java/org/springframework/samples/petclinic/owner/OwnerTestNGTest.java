280ae87fd2366f1dfd46173806c01b60f1fb395a
Mojo2354 <mhajo023@uottawa.ca>
2025-07-14 13:06:13 -0400

    yes

---
+++ src/test/java/org/springframework/samples/petclinic/owner/OwnerTestNGTest.java
@@ -0,0 +1,11 @@
+package org.springframework.samples.petclinic.owner;
+
+import org.testng.annotations.Test;
+
+public class OwnerTestNGTest {
+
+	@Test
+	public void sampleTest() {
+		assert 1 + 1 == 2;
+	}
+}
